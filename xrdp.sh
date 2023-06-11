9d3a83b3e10c208594291fdcfcc1de32bc087b53
BonBen365 <126295084+bonben365@users.noreply.github.com>
2023-06-11 13:01:41 +0700

    Create xrdp.sh

---
+++ xrdp.sh
@@ -0,0 +1,25 @@
+#Update the Distro installation and remove previous install of xrdp
+sudo apt-get update && sudo apt-get -y upgrade
+
+#Install a desktop environment and a rdp server 
+sudo apt-get purge xrdp
+sudo apt install -y xrdp xfce4 xfce4-goodies
+
+#Configure the xrdp server
+sudo sed -i 's/3389/3390/g' /etc/xrdp/xrdp.ini 
+sudo sed -i 's/max_bpp=32/#max_bpp=32\nmax_bpp=128/g' /etc/xrdp/xrdp.ini 
+sudo sed -i 's/xserverbpp=24/#xserverbpp=24\nxserverbpp=128/g' /etc/xrdp/xrdp.ini 
+echo xfce4-session > ~/.xsession
+
+sudo sed -i 's/test -x/#test -x/g' /etc/xrdp/startwm.sh
+sudo sed -i 's@exec /bin/sh@#exec /bin/sh@g' /etc/xrdp/startwm.sh
+echo '#xfce4' >> /etc/xrdp/startwm.sh
+echo 'startxfce4' >> /etc/xrdp/startwm.sh
+sed -i 's@.*if test -r /etc/profile.*@unset DBUS_SESSION_BUS_ADDRESS\n&@' /etc/xrdp/startwm.sh
+sed -i 's@.*if test -r /etc/profile.*@unset XDG_RUNTIME_DIR\n&@' /etc/xrdp/startwm.sh
+
+#Enable services on boot
+sudo systemctl enable xrdp
+sudo systemctl enable dbus
+
+
