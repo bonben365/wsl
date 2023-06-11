19e308a86f1a475e330639dd137801717ba0a1f9
BonBen365 <126295084+bonben365@users.noreply.github.com>
2023-06-11 16:39:55 +0700

    Update xrdp.sh

--- xrdp.sh
+++ xrdp.sh
@@ -6,9 +6,10 @@ sudo apt-get purge xrdp
 sudo apt install -y xrdp xfce4 xfce4-goodies
 
 #Configure the xrdp server
-sudo sed -i 's/3389/3390/g' /etc/xrdp/xrdp.ini 
-sudo sed -i 's/max_bpp=32/#max_bpp=32\nmax_bpp=128/g' /etc/xrdp/xrdp.ini 
-sudo sed -i 's/xserverbpp=24/#xserverbpp=24\nxserverbpp=128/g' /etc/xrdp/xrdp.ini 
+sudo sed -i 's/3389/3390/g' /etc/xrdp/xrdp.ini
+sudo sed -i 's/max_bpp=32/#max_bpp=32\nmax_bpp=128/g' /etc/xrdp/xrdp.ini
+sudo sed -i 's/xserverbpp=24/#xserverbpp=24\nxserverbpp=128/g' /etc/xrdp/xrdp.ini
+sudo sed -i 's/crypt_level=high/crypt_level=none/g' /etc/xrdp/xrdp.ini
 echo xfce4-session > ~/.xsession
 
 sudo sed -i 's/test -x/#test -x/g' /etc/xrdp/startwm.sh
