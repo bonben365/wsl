1fa295bdc8e656ade85a8a0099de4efe41c04d82
BonBen365 <126295084+bonben365@users.noreply.github.com>
2023-06-13 23:46:44 +0700

    Update enable-usb.sh

--- enable-usb.sh
+++ enable-usb.sh
@@ -1,7 +1,7 @@
-######################
+##############################################################################
 # Build WSL2 kernel with usb-storage support
 # menuconfig -> Device Drivers --> USB support ---> USB Mass Storage support
-######################
+##############################################################################
 sudo apt update && sudo apt upgrade -y && sudo apt install -y build-essential flex bison libgtk2.0-dev libelf-dev libncurses-dev autoconf libudev-dev libtool zip unzip v4l-utils libssl-dev python3-pip cmake git iputils-ping net-tools dwarves
 sudo mkdir /usr/src/$(uname -r)
 cd /usr/src/$(uname -r)
@@ -13,9 +13,7 @@ sudo make menuconfig
 sudo make -j$(nproc)
 sudo make modules_install -j$(nproc)
 sudo make install -j$(nproc)
-sudo cp -rf vmlinux /mnt/c/Sources/
-######################
-# Automount using udev to /media/usb[0 .. 7]
-######################
-sudo apt install usbmount 
-sudo /usr/lib/systemd/systemd-udevd &
+sudo mkdir /mnt/c/sources/
+sudo cp -rf vmlinux /mnt/c/sources/
+##############################################################################
+
