ca23d4b168091e5f02088a020f3810a992422a74
BonBen365 <126295084+bonben365@users.noreply.github.com>
2023-06-13 23:28:01 +0700

    Update enable-usb.sh

--- enable-usb.sh
+++ enable-usb.sh
@@ -2,11 +2,10 @@
 # Build WSL2 kernel with usb-storage support
 # menuconfig -> Device Drivers --> USB support ---> USB Mass Storage support
 ######################
-VERSION=$(uname -r)
 sudo apt update && sudo apt upgrade -y && sudo apt install -y build-essential flex bison libgtk2.0-dev libelf-dev libncurses-dev autoconf libudev-dev libtool zip unzip v4l-utils libssl-dev python3-pip cmake git iputils-ping net-tools dwarves
-sudo mkdir /usr/src1
-cd /usr/src1
-sudo git clone -b linux-msft-wsl-${VERSION} https://github.com/microsoft/WSL2-Linux-Kernel.git ${VERSION}-microsoft-standard && cd ${VERSION}-microsoft-standard
+sudo mkdir /usr/src/$(uname -r)
+cd /usr/src/$(uname -r)
+sudo git clone https://github.com/microsoft/WSL2-Linux-Kernel.git
 sudo cp /proc/config.gz config.gz
 sudo gunzip config.gz
 sudo mv config .config
