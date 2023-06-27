6ce14886ea3e15aba85bdf2f83fad13c61fe8094
BonBen365 <126295084+bonben365@users.noreply.github.com>
2023-06-27 21:45:53 +0700

    Create centos9.sh

---
+++ centos9.sh
@@ -0,0 +1,20 @@
+New-Item -Path "${env:LOCALAPPDATA}\Packages\centstr9" -ItemType Directory -Force
+Set-Location "${env:LOCALAPPDATA}\Packages\centstr9"
+
+$url = 'https://github.com/mishamosher/CentOS-WSL/releases/latest'
+$request = [System.Net.WebRequest]::Create($url)
+$response = $request.GetResponse()
+$tagUrl = $response.ResponseUri.OriginalString
+
+###Create the download link
+$downloadUrl = $tagUrl.Replace('tag', 'download') + '/' + "CentOS9-stream.zip"
+$filePath = "${env:LOCALAPPDATA}\Packages\centstr9\CentOS9-stream.zip"
+(New-Object Net.WebClient).DownloadFile($downloadUrl, $filePath)
+
+#Extraxt downloaded file to a .tar file
+Expand-Archive .\CentOS9-stream.zip
+Move-Item .\CentOS9-stream\rootfs.tar.gz rootfs.tar
+
+#Cleanup
+Remove-Item CentOS9-stream.zip
+Remove-Item .\CentOS9-stream\ -Recurse
