3c99c57c11e7e618c8599a7bffd1cf6384acb2c9
BonBen365 <126295084+bonben365@users.noreply.github.com>
2023-06-27 21:53:40 +0700

    Update centos9.sh

--- centos9.sh
+++ centos9.sh
@@ -1,5 +1,6 @@
 New-Item -Path "${env:LOCALAPPDATA}\Packages\centstr9" -ItemType Directory -Force
 Set-Location "${env:LOCALAPPDATA}\Packages\centstr9"
+Invoke-Item -Path "${env:LOCALAPPDATA}\Packages\centstr9"
 
 $url = 'https://github.com/mishamosher/CentOS-WSL/releases/latest'
 $request = [System.Net.WebRequest]::Create($url)
@@ -11,7 +12,7 @@ $downloadUrl = $tagUrl.Replace('tag', 'download') + '/' + "CentOS9-stream.zip"
 $filePath = "${env:LOCALAPPDATA}\Packages\centstr9\CentOS9-stream.zip"
 (New-Object Net.WebClient).DownloadFile($downloadUrl, $filePath)
 
-Write-Host 'Downloading CentOS Stream 9from GitHub...'
+Write-Host 'Downloading CentOS Stream 9 from GitHub...'
 
 #Extraxt downloaded file to a .tar file
 Expand-Archive .\CentOS9-stream.zip
