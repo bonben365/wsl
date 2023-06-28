4557898100073d0dc5924dbe7ee81d7e7872df82
BonBen365 <126295084+bonben365@users.noreply.github.com>
2023-06-28 08:58:17 +0700

    Update centos8-9.sh

--- centos8-9.sh
+++ centos8-9.sh
@@ -1,12 +1,11 @@
 yum upgrade
-reboot
-dnf install epel-release
-dnf install rpmconf
-dnf install yum-utils
+dnf install epel-release -y
+dnf install rpmconf -y
+dnf install yum-utils -y
 rpmconf -a  # answer "n" to both things
 package-cleanup --leaves
 package-cleanup --orphans
-dnf install http://mirror.stream.centos.org/9-stream/BaseOS/x86_64/os/Packages/centos-stream-repos-9.0-9.el9.noarch.rpm http://mirror.stream.centos.org/9-stream/BaseOS/x86_64/os/Packages/centos-stream-release-9.0-9.el9.noarch.rpm http://mirror.stream.centos.org/9-stream/BaseOS/x86_64/os/Packages/centos-gpg-keys-9.0-9.el9.noarch.rpm
+dnf -y install http://mirror.stream.centos.org/9-stream/BaseOS/x86_64/os/Packages/centos-stream-repos-9.0-9.el9.noarch.rpm http://mirror.stream.centos.org/9-stream/BaseOS/x86_64/os/Packages/centos-stream-release-9.0-9.el9.noarch.rpm http://mirror.stream.centos.org/9-stream/BaseOS/x86_64/os/Packages/centos-gpg-keys-9.0-9.el9.noarch.rpm
 curl -O https://dl.fedoraproject.org/pub/epel/epel-release-latest-9.noarch.rpm
 curl -O https://dl.fedoraproject.org/pub/epel/epel-next-release-latest-9.noarch.rpm
 rpm -Uvh *.rpm
